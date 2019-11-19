26585f726ce331d90abf11b4a4ab496b8b2180d9
Bilkovsky <a.bilkov@yandex.ru>
2019-11-20 02:34:24 +0300

    multiprocessing

--- io-test.py
+++ io-test.py
@@ -2,39 +2,55 @@ import sys
 import os
 import time
 from multiprocessing import Pool
+from functools import partial
+import threading
 
-def createFile(filename):
+
+def createFile(filename, pathtofiles, sizeoffiles, content):
     start = time.time()
-    with open(pathToFiles + filename, 'w') as file:
-        file.truncate(sizeOfFiles)
+    with open(pathtofiles + filename, 'w') as file:
+        file.truncate(sizeoffiles)
         file.write(content)
         file.close()
     end = time.time()
-    timeList.append(end - start)
+    return end - start
 
-try:
-    pathToFiles = ''
-    numberOfFiles = int(sys.argv[1])
-    sizeOfFiles = int(sys.argv[2])
-    if len(sys.argv) > 3:
-        pathToFiles = sys.argv[3] + '\\'
-    if not (os.path.exists(pathToFiles) or pathToFiles == ''):
-        print("Such directory doesnt exist")
-        exit(0)
-    content = input("Input your content pattern : ")
-    timeList = list()
-    appStart = time.time()
-    for i in range(0,numberOfFiles):
-        createFile('test' + str(i + 1) + '.txt')
-    appEnd = time.time()
-    timeList.sort()
-    print(str(numberOfFiles) + " files created")
-    print("Min : " + str(min(timeList)) + " Max : " + str(max(timeList)) + " Average : "
-          + str(sum(timeList)/len(timeList)) + " Total : " + str(appEnd - appStart))
 
-except IndexError:
-    print("Arguments count error, check your inputs")
-except ValueError:
-    print("Some of your parameters are incorrect!")
-except Exception as e:
-    print(e)
+if __name__ == '__main__':
+    try:
+        processes = 1
+        pathToFiles = ''
+        numberOfFiles = int(sys.argv[1])
+        sizeOfFiles = int(sys.argv[2])
+        if len(sys.argv) > 3:
+            pathToFiles = sys.argv[3] + '\\'
+            if len(sys.argv) > 4:
+                processes = int(sys.argv[4])
+        content = input("Input your content pattern : ")
+        if not (os.path.exists(pathToFiles) or pathToFiles == ''):
+            os.makedirs(pathToFiles)
+        timeList = list()
+        appStart = time.time()
+        names = []
+        pool = Pool(processes=processes)
+
+        for i in range(0, numberOfFiles):
+            names.append('test' + str(i + 1) + '.txt')
+
+        timeList.append(pool.map(partial(createFile, pathtofiles = pathToFiles, sizeoffiles = sizeOfFiles,content = content), names))
+        pool.close()
+        pool.join()
+        appEnd = time.time()
+        timeList.sort()
+        print(str(numberOfFiles) + " files created")
+        print(str(appEnd - appStart))
+        #print(str(len(timeList)))
+        #print("Min : " + str(min(timeList)) + " Max : " + str(max(timeList)) + " Average : "
+        #      + str(sum(timeList)/len(timeList)) + " Total : " + str(appEnd - appStart))
+
+    except IndexError:
+        print("Arguments count error, check your inputs")
+    except ValueError:
+        print("Some of your parameters are incorrect!")
+    except Exception as e:
+        print(e)
